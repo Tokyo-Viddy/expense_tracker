30828381bcfc6e21099c1c409a27f11e80b69617
David Gregorato <HydroSnake@gmail.com>
2021-09-13 11:20:06 +0900

    Fixed the spec helper file.

--- spec/acceptance/expense_tracker_api_spec.rb
+++ spec/acceptance/expense_tracker_api_spec.rb
@@ -2,3 +2,19 @@
 
 require 'rack/test'
 require 'json'
+
+module ExpenseTracker
+  RSpec.describe 'Expense Tracker API' do
+    include Rack::Test::Methods
+
+    it 'records submitted expenses' do
+      coffee = {
+        'payee' => 'Starbucks',
+        'amount' => 575,
+        'date' => '2021-09-12'
+      }
+
+      post '/expenses', JSON.generate(coffee)
+    end
+  end
+end
