e78219ebdea3f53fb78d28e19aca6fb29df6db02
David Gregorato <HydroSnake@gmail.com>
2021-09-13 12:51:04 +0900

    Added some tests, need to make app next.

--- spec/acceptance/expense_tracker_api_spec.rb
+++ spec/acceptance/expense_tracker_api_spec.rb
@@ -2,19 +2,50 @@
 
 require 'rack/test'
 require 'json'
+require_relative '../../app/api'
 
 module ExpenseTracker
   RSpec.describe 'Expense Tracker API' do
     include Rack::Test::Methods
 
-    it 'records submitted expenses' do
-      coffee = {
-        'payee' => 'Starbucks',
-        'amount' => 575,
-        'date' => '2021-09-12'
-      }
+    def app
+      ExpenseTracker::API.new
+    end
 
-      post '/expenses', JSON.generate(coffee)
+    def post_expense(expense)
+      post '/expenses', JSON.generate(expense)
+      expect(last_response.status).to eq(200)
+
+      parsed = JSON.parse(last_response.body)
+      expect(parsed).to include('expense_id' => a_kind_of(Integer))
+      expense.merge('id' => parsed['expense_id'])
+    end
+
+    it 'records submitted expenses' do
+      pending 'Need to persist expenses'
+      coffee = post_expense(
+        'payee'  => 'Starbucks',
+        'amount' => 575,
+        'date'   => '2021-09-12'
+      )
+
+      zoo = post_expense(
+        'payee'  => 'Zoo',
+        'amount' => 1525,
+        'date'   => '2021-09-12'
+      )
+
+      groceries = post_expense(
+        'payee'  => 'Summit',
+        'amount' => 9520,
+        'date'   => '2021-09-13'
+      )
+
+      get '/expenses/2021-09-12'
+      expect(last_response.status).to eq(200)
+
+      expenses = JSON.parse(last_response.body)
+      expect(expenses).to contain_exactly(coffee, zoo)
     end
   end
 end
